7a6e0525c534bf24cf7fabb5ad79a1122647a6ad
cielotds <tadascielo01@gmail.com>
2024-11-20 16:36:17 +0800

    Add files via upload

---
+++ Sari-Sari Store.sql
@@ -0,0 +1,38 @@
+/*create database Store; */
+
+/*create table ProductInfo (
+    ProductNum int primary key auto_increment,
+    ProductName varchar(50),
+    Price decimal(5, 2),
+    ProductType varchar(50),
+    PackagingSize varchar(10)
+); */
+
+/*insert into ProductInfo (ProductName, Price, ProductType, PackagingSize) values
+('Milo', 13.00, 'Powdered Beverage', '15g'),
+('Presto', 17.00, 'Biscuit', '30g'),
+('Bear Brand', 13.00, 'Powdered Beverage', '40g'),
+('Piattos', 20.00, 'Snack', '30g'),
+('Honey Butter', 51.00, 'Snack', '30g'),
+('Zonrox', 34.00, 'Bleach', '95ml'),
+('Close Up', 18.00, 'Toothpaste', '10g'),
+('Sunsilk', 12.00, 'Shampoo', '10g'),
+('UFC Ketchup', 46.00, 'Condiment', '100ml'),
+('Mountain Dew', 18.00, 'Beverage', '295ml'),
+('Tang', 19.00, 'Powdered Beverage', '19g'),
+('Head & Shoulders', 11.00, 'Shampoo', '10g'),
+('Rebisco', 12.00, 'Biscuit', '30g'),
+('Cheese Ring', 18.00, 'Snack', '30g'),
+('Datu Puti Soy Sauce', 25.00, 'Condiment', '100ml'),
+('Coffee Mate', 10.00, 'Beverage', '15g'),
+('Pasta Express', 12.00, 'Noodles', '60g'),
+('Century Tuna', 25.00, 'Canned Tuna', '150g'),
+('Nescafe', 10.00, 'Powdered Beverage', '15g'),
+('Golden Fiesta Oil', 100.00, 'Cooking Oil', '250ml'),
+('Downy', 10.00, 'Fabric Softener', '30g'),
+('Chuckie', 37.00, 'Beverage', '180ml'),
+('Colgate', 18.00, 'Toothpaste', '10g'),
+('Surf', 8.00, 'Detergent', '25g'),
+('Pepsi', 15.00, 'Beverage', '295ml'); */
+
+select * from ProductInfo;
